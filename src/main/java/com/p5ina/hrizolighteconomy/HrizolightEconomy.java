a0d989fadfa87114e50aaee14ee714db621eb4f3
p5ina <timatur242005@gmail.com>
2023-04-27 01:27:05 +0300

    init commit

---
+++ src/main/java/com/p5ina/hrizolighteconomy/HrizolightEconomy.java
@@ -0,0 +1,17 @@
+package com.p5ina.hrizolighteconomy;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HrizolightEconomy extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
