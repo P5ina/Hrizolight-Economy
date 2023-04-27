14f44856b88de729ff54b165174e16cff47484a9
Timur Turatbekov <timatur242005@gmail.com>
2023-04-27 14:54:39 +0300

    added logs

--- src/main/java/com/p5ina/hrizolighteconomy/HrizolightEconomy.java
+++ src/main/java/com/p5ina/hrizolighteconomy/HrizolightEconomy.java
@@ -1,17 +1,21 @@
 package com.p5ina.hrizolighteconomy;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class HrizolightEconomy extends JavaPlugin {
 
+
+
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        Bukkit.getLogger().info("[Hrizolight Economy] Enabled!");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        Bukkit.getLogger().info("[Hrizolight Economy] Disabled!");
     }
 }
